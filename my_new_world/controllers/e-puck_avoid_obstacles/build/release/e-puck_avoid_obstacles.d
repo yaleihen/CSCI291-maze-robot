6bfd54782f83045c993b8bc2382d2dd971210daa
yaleihen <muth@yaleihen.com>
2024-10-06 20:13:34 +0400

    initial initial commit

---
+++ my_new_world/controllers/e-puck_avoid_obstacles/build/release/e-puck_avoid_obstacles.d
@@ -0,0 +1,9 @@
+build/release/e-puck_avoid_obstacles.o: e-puck_avoid_obstacles.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/device.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/led.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h
