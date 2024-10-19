df70a7cfa7e9959cfbcdcec7982e62bf4cbaef8d
AIsmail17 <aabdelbadie6@gmail.com>
2024-10-19 11:48:46 +0400

    Maze navigation - Ahmed

--- my_new_world/controllers/e-puck_avoid_obstacles_ahmed_test/build/release/e-puck_avoid_obstacles_ahmed_test.d
+++ my_new_world/controllers/e-puck_avoid_obstacles_ahmed_test/build/release/e-puck_avoid_obstacles_ahmed_test.d
@@ -3,6 +3,5 @@ build/release/e-puck_avoid_obstacles_ahmed_test.o: \
  C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
  C:\Program\ Files\Webots/include/controller/c/webots/types.h \
  C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/light_sensor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h
--- my_new_world/controllers/e-puck_avoid_obstacles_ahmed_test/e-puck_avoid_obstacles_ahmed_test.c
+++ my_new_world/controllers/e-puck_avoid_obstacles_ahmed_test/e-puck_avoid_obstacles_ahmed_test.c
@@ -1,78 +1,69 @@
-#include <webots/robot.h>
-#include <webots/light_sensor.h>
-#include <webots/distance_sensor.h>
-#include <webots/motor.h>
 #include <stdio.h>
+#include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/distance_sensor.h>
 
-#define TIME_STEP 64  // Time step for simulation in ms
-#define MAX_SPEED 10
+#define TIME_STEP 64
+#define MAX_SPEED 6.28
+#define WALL_THRESHOLD 80.0
+#define TURN_THRESHOLD 100.0
 
-void move_forward();
-void turn_left();
-void turn_right();
-void stop();
+// Function Protoypes
+void move(double left_speed, double right_speed);
 
+// Initialize motors and sensors
+WbDeviceTag left_motor, right_motor; // Motors
+WbDeviceTag ds_left, ds_right, ds_front; // Distance Sensors
+
+// Main function
 int main(int argc, char **argv) {
-  // Initialize the Webots API
   wb_robot_init();
 
-  move_forward();
-  turn_left();
-  move_forward();
-  turn_right();
-  stop();
-
-  // Define and enable the light sensor (light sensor)
-  WbDeviceTag light_sensor = wb_robot_get_device("light sensor");  // Replace "lts" with the actual name of the light sensor
-  wb_light_sensor_enable(light_sensor, TIME_STEP);
-
-  // Main loop
+  // Get the motor devices
+  left_motor = wb_robot_get_device("left wheel motor");
+  right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_position(right_motor, INFINITY);
+  
+  // Initialize distance sensors
+  ds_left = wb_robot_get_device("ps5");  // Left Sensor
+  ds_right = wb_robot_get_device("ps2"); // Right Sensor
+  ds_front = wb_robot_get_device("ps7"); // Front Sensor
+  
+  wb_distance_sensor_enable(ds_left, TIME_STEP);
+  wb_distance_sensor_enable(ds_right, TIME_STEP);
+  wb_distance_sensor_enable(ds_front, TIME_STEP);
+   
   while (wb_robot_step(TIME_STEP) != -1) {
-    // Read the light sensor value
-    double light_value = wb_light_sensor_get_value(light_sensor);
+    // Read sensor values
+    // double left_dist = wb_distance_sensor_get_value(ds_left); // Removed, since it's not used
+    double right_dist = wb_distance_sensor_get_value(ds_right);
+    double front_dist = wb_distance_sensor_get_value(ds_front);
 
-    // Display the value
-    printf("Light Sensor Value: %f\n", light_value);
+/* FIX THIS PART
+    // Wall following Algorithm
+    if (front_dist > TURN_THRESHOLD) {
+        // No wall in front, move forward
+        if (right_dist < WALL_THRESHOLD) {
+            // Wall is close on the right, move forward while following the wall
+            move(MAX_SPEED, MAX_SPEED);
+        } else {
+            // No wall on the right, turn right slightly to align with the wall
+            move(MAX_SPEED * 0.8, MAX_SPEED);
+        }
+    } else {
+        // Wall directly in front, turn left to avoid the wall
+        move(-MAX_SPEED * 0.5, MAX_SPEED * 0.5);
+    }
   }
+*/
 
-  // Cleanup the Webots API
   wb_robot_cleanup();
-
   return 0;
 }
 
-void move_forward(){
-  // get the motor devices
-  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
-  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
-  // set the target position of the motors
-  wb_motor_set_position(left_motor, MAX_SPEED);
-  wb_motor_set_position(right_motor, MAX_SPEED);
-}
-
-void turn_left(){
-  // get the motor devices
-  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
-  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
-  // set the target position of the motors
-  wb_motor_set_position(left_motor, MAX_SPEED);
-  wb_motor_set_position(right_motor, -MAX_SPEED);
-}
-
-void turn_right(){
-  // get the motor devices
-  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
-  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
-  // set the target position of the motors
-  wb_motor_set_position(left_motor, -MAX_SPEED);
-  wb_motor_set_position(right_motor, MAX_SPEED);
-}
-
-void stop(){
-  // get the motor devices
-  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
-  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
-  // set the target position of the motors
-  wb_motor_set_position(left_motor, 0);
-  wb_motor_set_position(right_motor, 0);
+// Movement function
+void move(double left_speed, double right_speed) {
+  wb_motor_set_velocity(left_motor, left_speed);
+  wb_motor_set_velocity(right_motor, right_speed);
 }
