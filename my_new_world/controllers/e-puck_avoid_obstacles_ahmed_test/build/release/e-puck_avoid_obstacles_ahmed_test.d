00b2f3bb92cecff0fc6fb1c74a8d0cc453ca4340
AIsmail17 <aabdelbadie6@gmail.com>
2024-11-04 21:04:19 +0400

    deleted ahmed test file due to mistakes

--- my_new_world/controllers/e-puck_avoid_obstacles_ahmed_test/build/release/e-puck_avoid_obstacles_ahmed_test.d
+++
@@ -1,7 +0,0 @@
-build/release/e-puck_avoid_obstacles_ahmed_test.o: \
- e-puck_avoid_obstacles_ahmed_test.c \
- /home/march/everything/need/software/webots/include/controller/c/webots/robot.h \
- /home/march/everything/need/software/webots/include/controller/c/webots/types.h \
- /home/march/everything/need/software/webots/include/controller/c/webots/nodes.h \
- /home/march/everything/need/software/webots/include/controller/c/webots/motor.h \
- /home/march/everything/need/software/webots/include/controller/c/webots/distance_sensor.h
